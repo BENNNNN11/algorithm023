e80a73133d1a01509355f17a7799333db92105fc
GeekUni <15927191706@163.com>
2021-01-10 23:34:13 +1100

    Week02 homework completed

--- Week_02/[239]滑动窗口最大值.java
+++ Week_02/[239]滑动窗口最大值.java
@@ -62,33 +62,59 @@
 
 
 import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        if (n * k == 0) return new int[0];
-        if (k == 1) return nums;
-        Deque<Integer> deque = new LinkedList<>();
-        int[] res = new int[nums.length - k + 1];
-        for (int i = 1 - k, j = 0; j < n; j++, i++) {
-            if (i > 0 && nums[i - 1] == deque.peekFirst()) {
-                deque.removeFirst();
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] pair1, int[] pair2) {
+                return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1];
             }
-            while (!deque.isEmpty() && deque.peekLast() < nums[j]) {
-                deque.removeLast();
-            }
-            deque.addLast(nums[j]);
-            if (i >= 0) {
-                res[i] = deque.peekFirst();
-            }
+        });
+        for (int i = 0; i < k; ++i) {
+            pq.offer(new int[]{nums[i], i});
         }
-        return res;
+        int[] ans = new int[n - k + 1];
+        ans[0] = pq.peek()[0];
+        for (int i = k; i < n; ++i) {
+            pq.offer(new int[]{nums[i], i});
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            ans[i - k + 1] = pq.peek()[0];
+        }
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 // 1. 暴力
 //    O(n*k) TIME 超时
+
 // 2. DEQUE
 //  O(N+K) TIME
+//        int n = nums.length;
+//        if (n * k == 0) return new int[0];
+//        if (k == 1) return nums;
+//        Deque<Integer> deque = new LinkedList<>();
+//        int[] res = new int[nums.length - k + 1];
+//        for (int i = 1 - k, j = 0; j < n; j++, i++) {
+//            if (i > 0 && nums[i - 1] == deque.peekFirst()) {
+//                deque.removeFirst();
+//            }
+//            while (!deque.isEmpty() && deque.peekLast() < nums[j]) {
+//                deque.removeLast();
+//            }
+//            deque.addLast(nums[j]);
+//            if (i >= 0) {
+//                res[i] = deque.peekFirst();
+//            }
+//        }
+//        return res;
+
+// 3. PriorityQueue
+
+
+
 
