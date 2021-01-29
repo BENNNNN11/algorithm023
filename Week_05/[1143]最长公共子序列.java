00fc95c752c2c8897bb161ff7fd3c4b0ab93836b
GeekUni <15927191706@163.com>
2021-01-29 20:13:45 +1100

    Largest Common Subsequence

--- Week_05/[1143]最长公共子序列.java
+++ Week_05/[1143]最长公共子序列.java
@@ -47,13 +47,11 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
-        char[] t1 = text1.toCharArray();
-        char[] t2 = text2.toCharArray();
         if (m == 0 || n == 0) return 0;
         int[][] dp = new int[m+1][n+1];
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (t1[i-1] == t2[j-1]) {
+                if (text1.charAt(i-1) == t2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
                 } else {
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
